148730db48517b8a8a6e8f53dfee1cb400f63759
jhjaylim <jhjaylim@gmail.com>
2017-07-26 17:31:22 -0700

    Serve index.html

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,33 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
-
+var counter = 0;
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  console.log(++counter);
+  var index;
+  fs.readFile('web/public/index.html', 'utf8', (error, data) => {
+
+    if (error) {
+      console.log('error');
+    
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      
+      res.end(data);
+      
+    }
+
+  });
+  console.log(req.method);
+   // two cases
+    // check if it is valid url. 
+      // if valid
+
+
+  // requests will have url
+
+    // upon getting url, this request handler checks if the url has been archived yet
+
 };
